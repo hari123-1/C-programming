f32ff82bb93c375a03d5aecfbb590d5ae605a21c
hari123-1 <97817443+hari123-1@users.noreply.github.com>
2023-07-21 13:53:51 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,9 @@
+import java.util.scanner;
+class addition
+{
+Scanner ran=new Scanner(System.in);
+int a=ran.nextInt();
+int b=ran.nextInt();
+int c=a+b;
+System.out.println("Addition of two numbers "+c);
+}
